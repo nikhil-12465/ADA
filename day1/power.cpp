6d5da072ed9935d976faeee64b273ec0cbc6b065
nikhil-12465 <nikhilmishra993672@gmail.com>
2025-02-05 14:41:58 +0530

    power code is here of O(logn)

---
+++ day1/power.cpp
@@ -0,0 +1,33 @@
+// find the power of a number 
+#include<iostream>
+using namespace std;
+
+int power(int a, int b){
+    
+    int n = b;
+    if (n == 0) return 1;
+
+    else{
+        if(n%2 == 0){
+            return power(a, n/2) * power(a, n/2);
+        }
+        else{
+            return a * power(a,n/2) * power(a,n/2);
+        }
+    }
+}
+
+
+int main() {
+    int a, b;
+    cout<<"Enter a raise to power b for calculation"<<endl;
+    cin>>a;
+    cin>>b;
+    cout<<a<<"^"<<b<<" = "<<power(a,b)<<endl;
+
+    return 0;
+
+    
+
+
+}
