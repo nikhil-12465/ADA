474e8e7f4d297c85bec17fa0f7141118f3d96ce3
nikhil-12465 <nikhilmishra993672@gmail.com>
2025-02-11 12:16:59 +0530

    update

--- day2/cosim.cpp
+++ day2/cosim.cpp
@@ -9,68 +9,49 @@
 
 using namespace std;
 
+// Common words to ignore
+set<string> common_words = {"is", "am", "are", "was", "were", "be", "been", "being",
+                            "have", "has", "had", "having", "do", "does", "did",
+                            "a", "an", "the", "and", "of", "on", "in", "to", "with", 
+                            "for", "it", "at", "i"};  // Removed "i" as it's useful for meaning
 
-set<string> common_words = {"is", "am", "are", "was", "were", "be", "been", "being", 
-                         "have", "has", "had", "having", "do", "does", "did", 
-                         "a", "an", "the","and","of","on","in","to","with","for","it","at"};
-
-
-vector<string> create_vector_from_file(const string& filename) {              // function for read from file and created the vector
+vector<string> create_vector_from_file(const string& filename) {
     vector<string> words;
     ifstream file(filename);
 
     if (!file.is_open()) {
-        cout << "Could not open the file " << filename << endl;
-        return ;  
+        cerr << "Could not open the file: " << filename << endl;
+        exit(EXIT_FAILURE);  // Ensure program exits properly
     }
 
     string word;
     while (file >> word) {
-    
-        transform(word.begin(), word.end(), word.begin(), ::tolower);             // Convert each word  to lowercase 
-        
-        
-        word.erase(remove_if(word.begin(), word.end(), ::ispunct), word.end());   // remove the pantuations if any exits 
+        transform(word.begin(), word.end(), word.begin(), ::tolower);
+        word.erase(remove_if(word.begin(), word.end(), ::ispunct), word.end());
 
-       
-        if (common_words.find(word) == common_words.end()) {                       // skip that are the common words 
-            words.push_back(word);  
+        if (!word.empty() && common_words.find(word) == common_words.end()) {
+            words.push_back(word);
         }
     }
 
-    file.close(); 
+    file.close();
     return words;
 }
 
-
-vector<int> create_word_frequency_vector(const vector<string>& words, const set<string>& uniqueWords) {            // function  for creation of the word frequency vector which is used for find the frequency of the unique words and make their vector 
+vector<int> create_word_frequency_vector(const vector<string>& words, const set<string>& uniqueWords) {
     vector<int> wordFreq(uniqueWords.size(), 0);
+    size_t index = 0;
 
-   for (const string& word : words) {
-   
-    if (uniqueWords.count(word) > 0) {
-       
-        size_t index = 0;
-        for (const string& uniqueWord : uniqueWords) {
-            if (uniqueWord == word) {
-                wordFreq[index]++;
-                break;  
-            }
-            index++;
-        }
+    for (const string& word : uniqueWords) {
+        wordFreq[index] = count(words.begin(), words.end(), word);
+        index++;
     }
-}
-
-
+    
     return wordFreq;
 }
 
-
-double cosine_similarity(const vector<int>& vec1, const vector<int>& vec2) {     // function for the cosine similarity 
-    double dotProd = 0.0;
-    double magVec1 = 0.0;
-    double magVec2 = 0.0;
-
+double cosine_similarity(const vector<int>& vec1, const vector<int>& vec2) {
+    double dotProd = 0.0, magVec1 = 0.0, magVec2 = 0.0;
 
     for (size_t i = 0; i < vec1.size(); ++i) {
         dotProd += vec1[i] * vec2[i];
@@ -78,32 +59,44 @@ double cosine_similarity(const vector<int>& vec1, const vector<int>& vec2) {    
         magVec2 += vec2[i] * vec2[i];
     }
 
+    if (magVec1 == 0.0 || magVec2 == 0.0) {             // condition for avoiding division by zero 
+        return 0.0;  
+    }
+
     return dotProd / (sqrt(magVec1) * sqrt(magVec2));
 }
 
-
-
 int main() {
-    string file1 = "vector1.txt"; 
-    string file2 = "vector2.txt";  
+    string file1 = "vector1.txt";
+    string file2 = "vector2.txt";
 
-   
     vector<string> words1 = create_vector_from_file(file1);
     vector<string> words2 = create_vector_from_file(file2);
 
-   
-    set<string> uniqueWords;
-    uniqueWords.insert(words1.begin(), words1.end());                    // Create a set of unique words in both files
+    set<string> uniqueWords(words1.begin(), words1.end());
     uniqueWords.insert(words2.begin(), words2.end());
-
 
     vector<int> freqVector1 = create_word_frequency_vector(words1, uniqueWords);
     vector<int> freqVector2 = create_word_frequency_vector(words2, uniqueWords);
 
    
-    double similarity = cosine_similarity(freqVector1, freqVector2);           // calculate the similarity between the two frequency vectors 
+    cout << "Words in File1: ";
+    for (const string& word : words1) cout << word << " ";
+    cout << endl;
 
- 
+    cout << "Words in File2: ";
+    for (const string& word : words2) cout << word << " ";
+    cout << endl;
+
+    cout << "Frequency Vector 1: ";
+    for (int val : freqVector1) cout << val << " ";
+    cout << endl;
+
+    cout << "Frequency Vector 2: ";
+    for (int val : freqVector2) cout << val << " ";
+    cout << endl;
+
+    double similarity = cosine_similarity(freqVector1, freqVector2);
     cout << "Cosine Similarity between the vectors: " << similarity << endl;
 
     return 0;
